569703581dfd79f6a1dc01d8bb68584713347257
Alexander Krampe <38127951+AlexanderKrampe@users.noreply.github.com>
2019-11-23 10:54:49 +0100

    Add files via upload

---
+++ phyphox.ino
@@ -0,0 +1,21 @@
+#include <phyphoxBle.h> 
+BleServer server; //Server anlegen
+
+void setup()
+{
+  server.start(); //server starten...wer hätte es gedacht
+}
+
+void loop()
+{
+	//Spannungsteiler mit Poti auslesen 
+	int value = analogRead(A1);
+	float voltage = value * (3.3 / 1023.0);
+	//hier kann man prinzipipiell natürlich was anderes auslesen (Temp,...)
+
+ 
+	//Daten an Server senden und in phyphox sichtbar machen
+	server.write(voltage);
+	delay(50); //50ms warten
+	
+}
