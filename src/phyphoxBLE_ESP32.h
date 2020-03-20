342f3b5817cbcd8a363a6ed04afc1ff925cab4db
dorsel89 <d.dorsel@gmx.de>
2020-03-20 13:15:59 +0100

    remove redundant checksum

--- src/phyphoxBLE_ESP32.h
+++ src/phyphoxBLE_ESP32.h
@@ -2,6 +2,7 @@
 #ifndef PHYPHOXBLE_ESP32_H
 #define PHYPHOXBLE_ESP32_H
 
+#include <phyphoxBle.h>
 #include <BLEDevice.h>
 #include <BLEUtils.h>
 #include <BLEServer.h>
@@ -52,38 +53,6 @@ class BleServer
         void write(float&, float&, float&, float&, float&);
 };
 
-struct crc32
-{
-    static void generate_table(uint32_t(&table)[256])
-    {
-        uint32_t polynomial = 0xEDB88320;
-        for (uint32_t i = 0; i < 256; i++) 
-        {
-            uint32_t c = i;
-            for (size_t j = 0; j < 8; j++) 
-            {
-                if (c & 1) {
-                    c = polynomial ^ (c >> 1);
-                }
-                else {
-                    c >>= 1;
-                }
-            }
-            table[i] = c;
-        }
-    }
-
-    static uint32_t update(uint32_t (&table)[256], uint32_t initial, const uint8_t* buf, size_t len)
-    {
-        uint32_t c = initial ^ 0xFFFFFFFF;
-        const uint8_t* u = static_cast<const uint8_t*>(buf);
-        for (size_t i = 0; i < len; ++i) 
-        {
-            c = table[(c ^ u[i]) & 0xFF] ^ (c >> 8);
-        }
-        return c ^ 0xFFFFFFFF;
-    }
-};
 
 #endif
 #endif
--- src/phyphoxBle.h
+++ src/phyphoxBle.h
@@ -16,4 +16,40 @@
 #include "phyphoxBLE_ESP32.h"
 #endif
 
+#include "Arduino.h"
+
+struct crc32
+{
+    static void generate_table(uint32_t(&table)[256])
+    {
+        uint32_t polynomial = 0xEDB88320;
+        for (uint32_t i = 0; i < 256; i++) 
+        {
+            uint32_t c = i;
+            for (size_t j = 0; j < 8; j++) 
+            {
+                if (c & 1) {
+                    c = polynomial ^ (c >> 1);
+                }
+                else {
+                    c >>= 1;
+                }
+            }
+            table[i] = c;
+        }
+    }
+
+    static uint32_t update(uint32_t (&table)[256], uint32_t initial, const uint8_t* buf, size_t len)
+    {
+        uint32_t c = initial ^ 0xFFFFFFFF;
+        const uint8_t* u = static_cast<const uint8_t*>(buf);
+        for (size_t i = 0; i < len; ++i) 
+        {
+            c = table[(c ^ u[i]) & 0xFF] ^ (c >> 8);
+        }
+        return c ^ 0xFFFFFFFF;
+    }
+};
+
+
 #endif
