0b648460b8fefc3bf4fc6c0942940a7224ba9637
Dorsel89 <d.dorsel@gmx.de>
2020-07-23 10:47:20 +0200

    update description

--- examples/getDataFromSmartphone/getDataFromSmartphone.ino
+++ examples/getDataFromSmartphone/getDataFromSmartphone.ino
@@ -3,7 +3,9 @@ BleServer server("phyphox"); //Init server
 
 /*  In this example we will send the smartphones acceleroation in z direction to our mikrocontroller.
  *  If the acceleration is larger than 0.5*9.81m/s² the led is turned off.
- */ The needed phyphox experiment can be found in the examples folder.
+ *  The needed phyphox experiment can be found in the examples folder. Furthermore there is a qr code which can be used to import the 
+ *  experiment into phyhox.
+ */
 
 void receivedData();
 
