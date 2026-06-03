499568d2e2405a841641991f854b00ba24208768
Staacks <sebastian@staacks.de>
2026-06-04 01:19:23 +0200

    Update getDataFromSmartphone to use new input elements

--- examples/getDataFromSmartphone/getDataFromSmartphone.ino
+++ examples/getDataFromSmartphone/getDataFromSmartphone.ino
@@ -7,55 +7,107 @@ void receivedData();
 
 long lastTimestamp = 0;
 float blinkInterval = 100;
-bool led = true;
+bool ledState = true;
+bool ledToggleState = true;
+int mode = 1;
 
-void setup()
-{
-   Serial.begin(115200);
-   PhyphoxBLE::start();
-   PhyphoxBLE::configHandler=&receivedData;
-   pinMode(LED_BUILTIN, OUTPUT);
+void setup() {
+
+  Serial.begin(115200);
+  PhyphoxBLE::start();
+  PhyphoxBLE::configHandler=&receivedData;
+  pinMode(LED_BUILTIN, OUTPUT);
    
-   //Experiment
-   PhyphoxBleExperiment getDataFromSmartphone;
-   getDataFromSmartphone.setTitle("Set Blink Interval");
-   getDataFromSmartphone.setCategory("Arduino Experiments");
-   getDataFromSmartphone.setDescription("User can set Blink Interval of Mikrocontroller LED");
+  //Experiment
+  PhyphoxBleExperiment getDataFromSmartphone;
+  getDataFromSmartphone.setTitle("Set Blink Interval");
+  getDataFromSmartphone.setCategory("Arduino Experiments");
+  getDataFromSmartphone.setDescription("User can set Blink Interval of Mikrocontroller LED");
 
   //View
   PhyphoxBleExperiment::View firstView;
   firstView.setLabel("FirstView"); //Create a "view"
 
   //Edit
-  PhyphoxBleExperiment::Edit Interval;
-  Interval.setLabel("Interval");
-  Interval.setUnit("ms");
-  Interval.setSigned(false);
-  Interval.setDecimal(false);
-  Interval.setChannel(1);
+  PhyphoxBleExperiment::Edit intervalEdit;
+  intervalEdit.setLabel("Interval");
+  intervalEdit.setDefault(100);
+  intervalEdit.setUnit("ms");
+  intervalEdit.setSigned(false);
+  intervalEdit.setDecimal(false);
+  intervalEdit.setChannel(1);
 
-  firstView.addElement(Interval);
-  getDataFromSmartphone.addView(firstView);         //attach view to experiment
+  //Slider controlling the same value as the edit element (channel 1)
+  PhyphoxBleExperiment::Slider intervalSlider;
+  intervalSlider.setLabel("Interval");
+  intervalSlider.setDefault(100);
+  intervalSlider.setMinValue(50);
+  intervalSlider.setMaxValue(1000);
+  intervalSlider.setStepSize(50);
+  intervalSlider.setPrecision(0);
+  intervalSlider.setShowValue(false);
+  intervalSlider.setChannel(1);
+
+  //Toggle
+  PhyphoxBleExperiment::Toggle ledToggle;
+  ledToggle.setLabel("Enable LED");
+  ledToggle.setDefault(1);
+  ledToggle.setChannel(2);
+
+  //Dropdown
+  PhyphoxBleExperiment::Dropdown modeDropdown;
+  modeDropdown.setLabel("Mode");
+  const char* const optionLabels[] = {"Always On", "Blink", "Flash"};
+  const float optionValues[] = {1.0f, 2.0f, 3.0f};
+  modeDropdown.setOptions(3, optionLabels, optionValues);
+  modeDropdown.setChannel(3);
+
+  firstView.addElement(intervalEdit);
+  firstView.addElement(intervalSlider);
+  firstView.addElement(ledToggle);
+  firstView.addElement(modeDropdown);
+  getDataFromSmartphone.addView(firstView);              //attach view to experiment
   PhyphoxBLE::addExperiment(getDataFromSmartphone);      //attach experiment to server
 }
 
 
-void loop()
-{
+void loop() {
    PhyphoxBLE::poll(); //Only required for the Arduino Nano 33 IoT, but it does no harm for other boards.
 
-    if(millis()-lastTimestamp>blinkInterval){
-      lastTimestamp = millis();
-      led=!led;
-      digitalWrite(LED_BUILTIN, led);
-      }
+   switch (mode) {
+      case 1: //Always On if toggle is on
+         ledState = ledToggleState;
+         break;
+      case 2: //Blink, i.e. toggle after each interval
+         if (millis() - lastTimestamp > blinkInterval) {
+            lastTimestamp = millis();
+            ledState = (!ledState) && ledToggleState;
+         }
+         break;
+      case 3: //Flash, i.e. Short flash every interval
+         ledState = (millis() - lastTimestamp < 10) && ledToggleState;
+         if (millis() - lastTimestamp > blinkInterval) {
+            lastTimestamp = millis();
+         }
+         break;
+   }
+   digitalWrite(LED_BUILTIN, ledState);
 }
 
-void receivedData(){
+void receivedData() {
    float receivedInterval;
-   PhyphoxBLE::read(receivedInterval);
-   if(receivedInterval>0){
-    blinkInterval =receivedInterval;
-    }
+   float receivedToggleState;
+   float receivedMode;
+   PhyphoxBLE::read(receivedInterval, receivedToggleState, receivedMode);
+   if (blinkInterval > 0) {
+     blinkInterval = receivedInterval;
+   }
+   ledToggleState = (receivedToggleState != 0);
+   mode = receivedMode;
+   Serial.print("Mode: ");
+   Serial.print(mode);
+   Serial.print(", LED state: ");
+   Serial.print(ledToggleState);
+   Serial.print(", interval: ");
    Serial.println(blinkInterval);
 }
